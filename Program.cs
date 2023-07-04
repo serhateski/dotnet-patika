9b48ada82d9f2616879368cee76b9062c3ee91d7
Serhat ESKİ <serhat@kaledmn.kaleiplik.com>
2023-07-04 15:38:48 +0300

    kayit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello World");
+
+byte b = 5;
+sbyte sb = 5;
+
+short s = 5;
+ushort us = 5;
+
+Int16 i16 = 5;
+Int128 i = 5;
+Int32 i32 = 5;
+Int64 i64 = 5;
+
+uint ui = 2;
+
+long l = 2;
+ulong ul = 2;
+
+float f = 5;
+double db = 5.0;
+
+char c = '2';
+bool bl = true;
+
+DateTime dt = DateTime.Now;
+Console.WriteLine(dt);
+
+string str1 = "20";
+int str2 = 20;
+
+string yeniDeger = str1 + str2.ToString();
+Console.WriteLine(yeniDeger);
+
+
+
+
+
+
+
+
